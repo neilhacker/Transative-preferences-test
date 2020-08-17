fa90aa10dd6ee2c2f85bba1d73f8051d65d714b6
Neil Hacker <neilhacker@Neils-MBP.router.local>
2020-08-17 17:26:14 +0100

    Update transitive_pref.py
    update

--- transitive_pref.py
+++ transitive_pref.py
@@ -11,7 +11,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from collections import defaultdict 
 
-options = ['Rio', 'tokyo', 'venice', 'rome','NYC','oslo','athens', 'antartica']
+options = ['Rio', 'tokyo', 'venice', 'rome','NYC','oslo','tulum', 'maldives']
 
 x = itertools.combinations(options,2)
 
@@ -184,6 +184,9 @@ plt.show()
 G_all = nx.DiGraph(all_edges) #find all groups of strongly connected components
 all_SSC_nodes = [x for x in nx.kosaraju_strongly_connected_components(G_all) if len(x) > 1]
 
+# rare issue: you have >2 indifference sets that are SCC but the only cycle does
+# not go through and indifferent edges so no cycle is found
+
 if len(indif_node_groups) > 1: #only triggers if there multiple indifference sets one could link between
     if len(all_SSC_nodes) < len(indif_node_groups):# only triggers if at least 2 indifference sets are linked 
         #sets up list to hold lists of indifference sets that are in a cycle
@@ -218,22 +221,24 @@ if len(indif_node_groups) > 1: #only triggers if there multiple indifference set
         # sets that are joined
         good_cycles = [] 
         for group_of_indif_sets in indif_sets_in_same_SCC:
-            good_cycle = [options]
+            good_cycle = []
             for cycle in cyc:
                 count = 0
                 for node_sets in group_of_indif_sets:
                     if any(i in cycle for i in node_sets) == True:
                         count += 1
-                if count == len(group_of_indif_sets) and len(cycle) < len(good_cycle[0]):
-                    good_cycle = cycle
-                    if len(cycle) == 3:
-                        break
+                if count == len(group_of_indif_sets):
+                    #below makes sure that at least one edge in the cycle goes within an indiff set
+                    if any(edge in nodes_to_edges(cycle) for edge in indif_edge_groups) == True:
+                        good_cycle = cycle
+                        break        
             good_cycles.append(good_cycle)
                 
                 
         edge_cyclez = []
-        for node in good_cycles:
-            edge_cyclez.append(nodes_to_edges(node))
+        if len(good_cycles[0]) > 0:
+            for node in good_cycles:
+                edge_cyclez.append(nodes_to_edges(node))
 
         directed_edges_graph3 = directed_edges.copy()
         indif_edges_graph3 = indif_edges.copy()
@@ -299,29 +304,37 @@ if len(indif_node_groups) > 1: #only triggers if there multiple indifference set
 G_indif = nx.DiGraph(indif_edges)
 indif_node_groups4 = [x for x in nx.kosaraju_strongly_connected_components(G_indif) if len(x) > 1]
 
+G_al = nx.DiGraph(all_edges)
+cyc = list(nx.simple_cycles(G_al))
+cyc = sorted(cyc, key=len)
+
 # finds shortest cycles for each indif set that contain an edge from indif set
 good_cyc = []
 for group in indif_node_groups4:
-    print('indif group:',group)
     good_cycle = []
     end = False
     for cycle in cyc:
+        cycle_copy = cycle.copy()
+        cycle_copy.append(cycle_copy[0])
         if end == False:
-            for i in range(len(cycle)-1):
-                pair = [cycle[i], cycle[i+1]]
+            for i in range(len(cycle_copy)-1):
+                pair = [cycle_copy[i], cycle_copy[i+1]]
                 if all(node in group for node in pair) == True:
-                    if all(node in group for node in cycle) == False:
-                        good_cycle = cycle
+                    if all(node in group for node in cycle_copy) == False:
+                        good_cycle = cycle_copy[0:-1]
                         end = True
+                        print(good_cycle)
                     break
     good_cyc.append(good_cycle)
 
-        
 edge_cyc = []
-for node in good_cyc:
-    edge_cyc.append(nodes_to_edges(node))
+if len(good_cyc[0]) or len(good_cyc)> 0:
+    for nodes in good_cyc:
+        if len(nodes) > 0:
+            edge_cyc.append(nodes_to_edges(nodes))
 
 directed_edges_graph4 = directed_edges.copy()
+print(directed_edges_graph4)
 indif_edges_graph4 = indif_edges.copy()
 for cycle in edge_cyc:
     for edge in cycle:
